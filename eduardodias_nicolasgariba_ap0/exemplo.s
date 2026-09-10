3c68271558c8ef758f5c1f54f310ec4ea0d6d130
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2026-09-09 22:44:21 -0300

    AP0 (#1)
    * feat: add vetores.ini file
    
    * feat: setup with initialization file
    
    * feat: create .gitignore
    
    * feat: add diagram file
    
    * Delete eduardodias_nicolasgariba_ap0/EventRecorderStub.scvd
    
    * Revert "feat: add diagram file"
    
    This reverts commit 57a1c30389aae6596c7d959314a45d71f86c27c5.
    
    * Revert "feat: create .gitignore"
    
    This reverts commit 2df3367a29b66c1de0550848747f4b54dcf8d0fc.
    
    * Revert "feat: setup with initialization file"
    
    This reverts commit 79ee88069ac81a6bc11964976d95be9cc45b85fa.
    
    * feat: add gitignore
    
    * chore: changes generated by compilation
    
    * feat: update gitignore
    
    * Delete eduardodias_nicolasgariba_ap0/Exemplo.uvoptx
    
    * feat: loop vasculhando ram
    
    * bubble
    
    * feat: ap0 done
    
    ---------
    
    Co-authored-by: Nicolas Rossi Gariba <nicolasrossigariba@gmail.com>

--- eduardodias_nicolasgariba_ap0/exemplo.s
+++ eduardodias_nicolasgariba_ap0/exemplo.s
@@ -8,6 +8,8 @@
 ; -------------------------------------------------------------------------------
 ; Declarações EQU - Defines
 ;<NOME>         EQU <VALOR>
+BEGIN_SCAN_ADDRESS EQU 0x20000400
+BEGIN_PALINDROME_ADDRESS EQU 0x20000600
 ; -------------------------------------------------------------------------------
 ; Área de Dados - Declarações de variáveis
 		AREA  DATA, ALIGN=2
@@ -25,16 +27,84 @@
 
 		; Se alguma função do arquivo for chamada em outro arquivo	
         EXPORT Start                ; Permite chamar a função Start a partir de 
-			                        ; outro arquivo. No caso startup.s
+									; outro arquivo. No caso startup.s
 									
 		; Se chamar alguma função externa	
-        ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
+		IMPORT BubbleSort              ; Permite chamar dentro deste arquivo uma 
 									; função <func>
 
 ; -------------------------------------------------------------------------------
 ; Função main()
-Start  
 ; Comece o código aqui <======================================================
 
+Start
+	LDR R8, =BEGIN_SCAN_ADDRESS
+	LDR R7, =BEGIN_PALINDROME_ADDRESS
+
+SearchRam ; inicio do loop vasculhando ram
+	LDRH R1, [R8], #0x2
+	CBZ R1, DoneSearchRam ; se achar zero acabou
+	
+	; verificar se eh palindromo
+	; =================	
+	MOV R0, R1
+	PUSH {R2}
+	BL Palindrome
+	POP {R2}
+	
+	; se R0 == 1 tem que armazenar R1 na RAM
+	CMP R0, #1
+	ITT EQ
+	ADDEQ R2, R2, #1 ; R2 contador de palindromos
+	STRHEQ R1, [R7], #0x2
+
+	; ================
+
+	B SearchRam
+	
+DoneSearchRam ; fim do loop (viu todos os valores do vetor na ram)
+	MOV R0, R2
+
+	B Done
+
+; ======== funcoes auxiliares =====
+
+Palindrome ; Retorna R0 = 1 se R0 for palindromo
+		   ; Retorna R0 = 0 caso contrario
+		   
+	MOV R1, #0 ; registrador que vai receber o numero invertido inicializado
+	MOV R2, R0 ; copia do numero original que vai ser "destruida"
+	MOV R3, #10 ; divisor
+
+LoopMirrorNum
+	CBZ R2, DoneLoopMirrorNum ; se R2 for zero acabou o loop
+	
+	UDIV R4, R2, R3 ; R4 = R2 / 10
+	MLS R5, R3, R4, R2 ; R5 = R2 - 10*R4
+	; R5 eh o resto da divisao de R2 por 10
+	
+	MUL R1, R1, R3
+	ADD R1, R1, R5
+	
+	MOV R2, R4	
+	
+	B LoopMirrorNum
+
+DoneLoopMirrorNum
+
+	CMP R0, R1
+	BEQ IsPalindrome
+	
+	MOV R0, #0
+	BX LR
+	
+IsPalindrome
+	MOV R0, #1
+	BX LR
+
+; ============
+
+Done
+	NOP
     ALIGN                           ; garante que o fim da seção está alinhada 
     END                             ; fim do arquivo
